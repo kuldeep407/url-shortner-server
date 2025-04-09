49868597b5e70bbf06351d6ea5091487320a812a
kuldeep407 <kuldeepchahar426@gmail.com>
2025-04-09 12:33:31 +0530

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://url-shortner-client-peach.vercel.app/",
+      "https://url-shortner-client-peach.vercel.app",
     ],
     credentials: true,
   })
